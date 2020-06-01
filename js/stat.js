1666ee92a512ce77633860cdfa031156ff271284
lesnik34 <lesnyak_2011@mail.ru>
2020-06-01 12:15:59 +0300

    Исправил ошибки

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,8 @@ var FONT_GAP = 15;
 var TEXT_WIDTH = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
+var CLOUD_COLOR = '#fff';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 
 
 var renderCloud = function (ctx, x, y, color) {
@@ -16,16 +18,8 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-
-  return maxElement;
+var getMaxElement = function (items) {
+  return Math.max.apply(null, items);
 };
 
 var getOptimalGap = function (players, width) {
@@ -33,36 +27,48 @@ var getOptimalGap = function (players, width) {
 };
 
 var getPlayerColor = function (plyaer) {
-  if (plyaer === 'Вы') {
-    return '#f00';
-  } else {
-    return 'hsl(240, ' + Math.floor(Math.random() * 101) + '%, 50%)';
-  }
+  return plyaer === 'Вы' ? '#f00' : 'hsl(240, ' + Math.floor(Math.random() * 101) + '%, 50%)';
 };
 
 var getOptimalX = function (players, i) {
   return CLOUD_X + 3 * GAP + (getOptimalGap(players, TEXT_WIDTH) + TEXT_WIDTH) * i;
 };
 
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.fillStyle = '#000';
+var drawText = function (ctx) {
   ctx.font = '16px PT Mono';
-
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 3 * GAP + FONT_GAP);
   ctx.fillText('Список результатов:', CLOUD_X + GAP, (7 * GAP) / 2 + 2 * FONT_GAP);
+};
 
-  var maxTime = getMaxElement(times);
+var drawBar = function (ctx, arr, i, times) {
+  var barOrdinate = CLOUD_HEIGHT - GAP - FONT_GAP - (times[i] * (BAR_HEIGHT / getMaxElement(times)));
+  ctx.fillRect(getOptimalX(arr, i), barOrdinate, BAR_WIDTH, times[i] * (BAR_HEIGHT / getMaxElement(times)));
+};
 
-  for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], getOptimalX(players, i), CLOUD_HEIGHT - GAP / 2);
-    ctx.fillText(Math.floor(times[i]), getOptimalX(players, i), CLOUD_HEIGHT - 2 * GAP - FONT_GAP - (times[i] * (BAR_HEIGHT / maxTime)));
+var drawHistogram = function (players, times, ctx) {
+  var nameOrdinate;
+  var timeOrdinate;
 
-    ctx.fillStyle = getPlayerColor(players[i]);
-    ctx.fillRect(getOptimalX(players, i), CLOUD_HEIGHT - GAP - FONT_GAP - (times[i] * (BAR_HEIGHT / maxTime)), BAR_WIDTH, times[i] * (BAR_HEIGHT / maxTime));
+  players.forEach(function (item, i, arr) {
+    nameOrdinate = CLOUD_HEIGHT - GAP / 2;
+    timeOrdinate = CLOUD_HEIGHT - 2 * GAP - FONT_GAP - (times[i] * (BAR_HEIGHT / getMaxElement(times)));
+
+    ctx.fillText(item, getOptimalX(arr, i), nameOrdinate);
+    ctx.fillText(Math.floor(times[i]), getOptimalX(arr, i), timeOrdinate);
+
+    ctx.fillStyle = getPlayerColor(item);
+    drawBar(ctx, arr, i, times);
     ctx.fillStyle = '#000';
-  }
+  });
+};
 
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+
+  ctx.fillStyle = '#000';
+
+  drawText(ctx);
+
+  drawHistogram(players, times, ctx);
 };
